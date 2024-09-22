a3c2c98d728ace9f083bbe94ea0aa716d2e93a62
VTU9730 <prathapsiddavatam@gmail.com>
2024-09-22 15:53:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <div className="header-item">
+          <i class="bi bi-box-arrow-in-down-left"></i>
+          <span>ChatGPT 4o mini</span>
+          <i class="bi bi-chevron-down"></i>
+        </div>
+        <div className="header-item">
+          <button className="btn btn-dark">Log in</button>
+          <button className="btn btn-outline-secondary">Sign up</button>
+        </div>
+      </div>
+      <div className="logo">
+        {/* <i class="bi bi-twitter"></i> */}
+        <img src="https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo-500x281.png" alt="logo-chatgpt" />
+      </div>
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="col-md-6">
+            <button className="btn btn-outline-secondary">
+              <strong>
+                <p>Suggest fun activities</p>
+              </strong>
+              <p>to help me make friends in a new city</p>
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button className="btn btn-outline-secondary">
+              <strong>
+                <p>Create a content calender</p>
+              </strong>
+              <p>for a TikTok account</p>
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button className="btn btn-outline-secondary">
+              <strong>
+                <p>Plan a mental health day</p>
+              </strong>
+              <p>to help me relax</p>
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button className="btn btn-outline-secondary">
+              <strong>
+                <p>Write a python code</p>
+              </strong>
+              <p>to automatesending daily email reports</p>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="search">
+        <input className="form-control input-search" type="text" placeholder="Message ChatGPT"></input>
+        <button className="btn"><i class="bi bi-arrow-up-circle"></i></button>
+      </div>
+      <div className="footer">
+        <p>ChatGPT can make mistakes. Check important info.</p>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function sum(a,b){
+    return a+b
+}
+
+describe("Sum function", () => {
+    it("return sum",() => {
+        const result = sum(3,4)
+        expect(result).toBe(7)
+    })
+})
+
+describe("App",()=>{
+    // it("text should render",()=>{
+    //     render(<App />)
+    //     const data = screen.getByText("App Component")
+    //     expect(data).toBeInTheDocument()
+    // })
+
+    // it("render button",()=>{
+    //     render(<App />)
+    //     const button = screen.getByRole("button")
+    // })
+
+    it("change login button",()=>{
+        render(<App />)
+        const loginButton = screen.getByRole("button",{name:"Login"})
+        fireEvent.click(loginButton)
+        const logoutButton = screen.getByText("Logout")
+        expect(logoutButton).toBeInTheDocument()
+    })
+
+    it("increment counter",() => {
+        render(<App />)
+        const incrementButton = screen.getByText("+")
+        fireEvent.click(incrementButton)
+        fireEvent.click(incrementButton)
+        
+        const counter = screen.getByText(/51/)
+        console.log(counter);
+        expect(counter).toBeInTheDocument()
+    })
+})
